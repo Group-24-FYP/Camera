2eae6415d8f962cf03ea5dffb35ea272dfa400dd
MayooranT <mayoo4234@gmail.com>
2023-03-23 23:01:46 +0530

    recording video

--- recorder.py
+++ recorder.py
@@ -2,15 +2,23 @@ import cv2
 import imutils
 from imutils.video import VideoStream
 rtsp_url = "rtsp://root:entc@192.168.0.101/axis-media/media.amp"
-video_stream = VideoStream(rtsp_url).start()
+#video_stream = VideoStream(rtsp_url).start()
+video_stream = cv2.VideoCapture(rtsp_url)
 
-writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (576,720))
+# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+# writer= cv2.VideoWriter('basicvideo.mp4',fourcc, 30, (576,720))
+
+w = video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)
+h = video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter('output.mp4',fourcc, 30, (576,720))
 
 while True:
     frame = video_stream.read()
     writer.write(frame)
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == 27:
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
 
 video_stream.stop()
--- test.py
+++ test.py
@@ -1,19 +1,21 @@
 import cv2
-import imutils
-from imutils.video import VideoStream
+
 rtsp_url = "rtsp://root:entc@192.168.0.101/axis-media/media.amp"
-video_stream = VideoStream(rtsp_url).start()
+cap = cv2.VideoCapture(rtsp_url)
+
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'mp4v') , 30, (width,height))
 
 while True:
-    frame = video_stream.read()
-    if frame is None:
-        continue
-
-    frame = imutils.resize(frame,width=720,height=576)
-    cv2.imshow('AsimCodeCam', frame)
+    ret,frame= cap.read()
+    writer.write(frame)
+    cv2.imshow('frame', frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
+cap.release()
+writer.release()
 cv2.destroyAllWindows()
-video_stream.stop()
